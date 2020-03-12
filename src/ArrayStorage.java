58e0b074d2e717fffe7b599f18e2ff0d19845e3f
Alexey <groznovaa@gmail.comls>
2020-03-13 01:33:45 +0500

    modified StorageArray

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -6,10 +6,10 @@ public class ArrayStorage {
     int len = 0;
 
     void clear() {
-        for (int i = 0; i < storage.length - 1; i++) {
-            if (storage[i] == null) break;
+        for (int i = 0; i < len; i++) {
             storage[i] = null;
         }
+        len = 0;
     }
 
     void save(Resume r) {
@@ -18,49 +18,41 @@ public class ArrayStorage {
     }
 
     Resume get(String uuid) {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null) break;
-            if (storage[i].uuid == uuid) return storage[i];
+        for (int i = 0; i < len; i++) {
+            if (storage[i].uuid == uuid) {
+                return storage[i];
+            }
         }
         return null;
     }
 
 
     void delete(String uuid) {
-        int lastNumber = 0;
-        for (int i = 0; i < storage.length - 1; i++) {
-            if (storage[i] == null) break;
+        boolean isNumberFind = false;
+
+        for (int i = 0; i < len; i++) {
             if (storage[i].uuid == uuid) {
-                for (int j = i; j < storage.length - 1; j++) {
-                    if (storage[j + 1] == null) {
-                        lastNumber = j;
-                        break;
-                    }
-                    storage[j] = storage[j + 1];
-                }
-                storage[lastNumber] = null;
-                break;
+                isNumberFind = true;
+            }
+            if (isNumberFind) {
+                storage[i] = storage[i + 1];
             }
         }
+        storage[len - 1] = null;
+        len = len - 1;
     }
+
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        int i = 0;
-        for (; i < storage.length - 1; i++) {
-            if (storage[i] == null) break;
-        }
-        Resume[] storageClone = new Resume[i];
-        System.arraycopy(storage, 0, storageClone, 0, i);
-        return storageClone;
+        Resume[] resumes = new Resume[len];
+        System.arraycopy(storage, 0, resumes, 0, len);
+        return resumes;
     }
 
     int size() {
-        for (int i = 0; i < storage.length - 1; i++) {
-            if (storage[i] == null) return i;
-        }
-        return 0;
+        return len;
     }
 }
