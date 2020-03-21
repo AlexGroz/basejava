88783b52cf181027d2c84888f47079dde4a5f0ab
Alexey <groznovaa@gmail.comls>
2020-03-21 22:45:15 +0500

    Add Arrays invocation

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -2,6 +2,8 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.model.Resume;
 
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -10,9 +12,7 @@ public class ArrayStorage {
     private int len = 0;
 
     public void clear() {
-        for (int i = 0; i < len; i++) {
-            storage[i] = null;
-        }
+        Arrays.fill(storage, 0, len, null);
         len = 0;
     }
 
@@ -60,11 +60,7 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     public Resume[] getAll() {
-        Resume[] result = new Resume[len];
-        for (int i = 0; i < len; i++) {
-            result[i] = storage[i];
-        }
-        return result;
+        return Arrays.copyOfRange(storage, 0, len);
     }
 
     public int size() {
